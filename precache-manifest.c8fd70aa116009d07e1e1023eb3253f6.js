2ddbfc7e8cbca8ce12cccf6a08106bea452a3d16
Parneet Kaur <parneet.p.kaur@oracle.com>
2020-04-05 17:50:15 -0700

    Initial

---
+++ precache-manifest.c8fd70aa116009d07e1e1023eb3253f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1818f0130772bcb19dbaaa2e1bc2f55e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d889ae23a45fbe90a89",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "cadb06388f088ca2b0d4",
+    "url": "/static/css/main.3b91e165.chunk.css"
+  },
+  {
+    "revision": "8d889ae23a45fbe90a89",
+    "url": "/static/js/2.0a39001d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0a39001d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cadb06388f088ca2b0d4",
+    "url": "/static/js/main.62d89cab.chunk.js"
+  },
+  {
+    "revision": "5de21ce63f37e147c046",
+    "url": "/static/js/runtime-main.a27ba908.js"
+  }
+]);
